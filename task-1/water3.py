5dfce3b350d782e6d6d7f0560fce969a268d38c9
arkaravitrak <144686152+arkaravitrak@users.noreply.github.com>
2023-09-11 21:10:23 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,24 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
-
+    return s[0,1] == 4 
 def successors(s):
     x, y, z = s
-    return []
+    #Try to pour from one to another  
+    t = 5 - y
+    if x > 0 and z > 0 and t > 0:
+         # x -> y
+        if x > t :
+            yield ((x - t, 5, z), t)
+        elif z > t:
+            yield ((x, 5 , ))
+    if z > 
+    t = 8 - x
+
+    t = 3 - z
+    if z > 0 and t > 0:
+        if z > t:
+            yield ((3, z-t ),t)
+        else:
+             yield (())
