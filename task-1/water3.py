77a957efb85efd7dd14d2b2bd0b59cfd5e8e7a52
LilPinnn <91402346+LilPinnn@users.noreply.github.com>
2023-09-12 17:53:18 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -17,7 +17,47 @@ def successors(s):
     t = 8 - x
 
     t = 3 - z
-    if z > 0 and t > 0:
+    if z > 0 and t > 0:def initial_state():
+    return (8, 0, 0)
+
+def is_goal(s):
+    return s[0] == 4 and s[1] == 4
+
+def successors(s):
+    actions = []
+    a, b, c = s
+
+    # Pour from the 8-liter bottle to the 5-liter bottle
+    if a > 0 and b < 5:
+        amount = min(a, 5 - b)
+        actions.append(((a - amount, b + amount, c), amount))
+
+    # Pour from the 8-liter bottle to the 3-liter bottle
+    if a > 0 and c < 3:
+        amount = min(a, 3 - c)
+        actions.append(((a - amount, b, c + amount), amount))
+
+    # Pour from the 5-liter bottle to the 8-liter bottle
+    if b > 0 and a < 8:
+        amount = min(b, 8 - a)
+        actions.append(((a + amount, b - amount, c), amount))
+
+    # Pour from the 5-liter bottle to the 3-liter bottle
+    if b > 0 and c < 3:
+        amount = min(b, 3 - c)
+        actions.append(((a, b - amount, c + amount), amount))
+
+    # Pour from the 3-liter bottle to the 8-liter bottle
+    if c > 0 and a < 8:
+        amount = min(c, 8 - a)
+        actions.append(((a + amount, b, c - amount), amount))
+
+    # Pour from the 3-liter bottle to the 5-liter bottle
+    if c > 0 and b < 5:
+        amount = min(c, 5 - b)
+        actions.append(((a, b + amount, c - amount), amount))
+
+    return actions  
         if z > t:
             yield ((3, z-t ),t)
         else:
